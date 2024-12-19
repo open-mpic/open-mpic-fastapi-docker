0459d848f1a96e660aea301ee7027aacbb1d547b
Dmitry Sharkov <dsharkov@gmail.com>
2024-12-19 02:57:44 -0500

    slowly reworking project to use toml and single Dockerfile

---
+++ api-implementation/tests/integration/testing_api_client.py
@@ -0,0 +1,21 @@
+import requests
+
+
+# not a mock client, but a real client for live API testing
+class TestingApiClient:
+    def __init__(self):
+        self.service_base_url = "localhost:8080"
+        self._session = requests.Session()
+
+    def get(self, url_suffix):
+        return self._session.get(self.service_base_url + '/' + url_suffix)
+
+    def post(self, url_suffix, data):
+        headers = {
+            'content-type': 'application/json',
+        }
+        response = self._session.post(self.service_base_url + '/' + url_suffix, headers=headers, data=data)
+        return response
+
+    def close(self):
+        self._session.close()
